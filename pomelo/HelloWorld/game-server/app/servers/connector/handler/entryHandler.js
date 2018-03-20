d6272d4a6357e7634e219d6bd9f0c1d73fae1fae
alexandra-prisecaru <alexandra.prisecaru@cs.pub.ro>
2018-03-20 16:41:56 +0200

    pomelo HelloWorld test

---
+++ pomelo/HelloWorld/game-server/app/servers/connector/handler/entryHandler.js
@@ -0,0 +1,61 @@
+module.exports = function(app) {
+  return new Handler(app);
+};
+
+var Handler = function(app) {
+  this.app = app;
+};
+
+/**
+ * New client entry.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.entry = function(msg, session, next) {
+  console.log("received session stuff: ", session);
+  console.log("received msg stuff: ", msg);
+  const no = parseInt(msg.number);
+  var sum = 0;
+  // if (!isNaN(no)) {
+    for (var i = 1; i <= no; i++)
+      sum += i;
+  // }
+  const resultMessage = (sum === 0 ?
+    'Enter a valid number.' : 'Computed sum is: [ '+ sum +  ' ].');
+  next(null, {code: 200, msg: resultMessage});
+};
+
+/**
+ * Publish route for mqtt connector.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.publish = function(msg, session, next) {
+	var result = {
+		topic: 'publish',
+		payload: JSON.stringify({code: 200, msg: 'publish message is ok.'})
+	};
+  next(null, result);
+};
+
+/**
+ * Subscribe route for mqtt connector.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.subscribe = function(msg, session, next) {
+	var result = {
+		topic: 'subscribe',
+		payload: JSON.stringify({code: 200, msg: 'subscribe message is ok.'})
+	};
+  next(null, result);
+};
